1434c17cd5cc18463190787cafd8fe904c79070f
Akshat Gupta <guptaakshat2309@gmail.com>
2025-12-18 12:31:37 +0530

    some trial runs

--- Lzr-u92x/commonPoints_reader.py
+++ Lzr-u92x/commonPoints_reader.py
@@ -8,7 +8,7 @@ import sys
 # ==========================================
 # CONFIGURATION
 # ==========================================
-SERIAL_PORT = 'COM4'  # Change to your port
+SERIAL_PORT = 'COM5'  # Change to your port
 BAUD_RATE = 460800  # Standard LZR U921 baud rate
 POINTS_PER_SCAN = 274  # Standard LZR resolution
 FOV_DEGREES = 96.0  # Field of View
